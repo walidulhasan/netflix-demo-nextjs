34ee60ef03c3d7b8be21a94860cec703b5d5dc14
walidulhasan <walidulhasanboniamin@gmail.com>
2022-11-27 09:57:19 +0600

    day 1 end

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  swcMinify:true,
+  optimizeFonts:true,
+ 
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+ 
+};
 
 module.exports = nextConfig
