22bde76d826ca5aa82c214ebba9535ceaa509a8b
walidulhasan <walidulhasanboniamin@gmail.com>
2022-11-28 10:17:36 +0600

    quick state change

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,9 @@ const Home = ({
    const showModal=useRecoilValue(modalState);
   if (loading) return null;
   return (
-    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
+    <div className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+      showModal && '!h-screen overflow-hidden'
+    }`}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
